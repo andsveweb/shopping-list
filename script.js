c765f1065d63a830530234e9f7a58c8c0ef3f559
andreas svensson <andsveweb@gmail.com>
2023-06-19 10:08:32 +0200

    added function delete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 function addItem(e) {
   e.preventDefault();
@@ -15,10 +16,10 @@ function addItem(e) {
   li.appendChild(document.createTextNode(newItem));
 
   const button = createButton('remove-item btn-link text-red');
-  li.appendChild(button)
+  li.appendChild(button);
   itemList.appendChild(li);
 
-  itemInput.value = ''
+  itemInput.value = '';
 }
 function createButton(classes) {
   const button = document.createElement('button');
@@ -34,5 +35,18 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
 //Event listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
